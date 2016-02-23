82b46495102ebeced1478e8c0fbfefa55c74606e
Keith Lazuka <kelazuka@microsoft.com>
2016-02-22 20:37:47 -0800

    Got basic changelog working

--- src/frontend/Overview/Component/Changes.elm
+++ src/frontend/Overview/Component/Changes.elm
@@ -28,7 +28,6 @@ import Overview.Slider as Slider
 import Page.Context as Ctx
 import Parse.Type as Type
 import Utils.Path exposing ((</>))
-import Utils.ProximityTree as Prox
 
 
 
@@ -37,9 +36,6 @@ import Utils.ProximityTree as Prox
 
 type alias Model =
     { history : History.History
-    , versions : Prox.ProximityTree Vsn.Version
-    , slider1 : Slider.Model
-    , slider2 : Slider.Model
     , docs : Dict.Dict Vsn.Version Docs
     }
 
@@ -52,128 +48,25 @@ type Docs
 
 init : Ctx.OverviewContext -> History.History -> ( Model, Fx.Effects Action )
 init context history =
-  let
-    proxTree =
-      Prox.map .version (Prox.fromList (toFloat << .date) history)
-
-    spacings =
-      timelineSpacingByVersion history
-
-    spacer version =
-      Maybe.withDefault 0.0 (Dict.get version spacings)
-
-    optimizedTree =
-      Prox.optimizeSpacing spacer proxTree
-
-    (penultimate, ultimate) =
-      latestInterestingVersions history
-  in
-    ( Model
-        history
-        optimizedTree
-        (Slider.init (Prox.lookup penultimate optimizedTree))
-        (Slider.init (Prox.lookup ultimate optimizedTree))
-        (Dict.fromList [ penultimate => Loading, ultimate => Loading ])
-    , Fx.batch
-        [ loadDocs context penultimate
-        , loadDocs context ultimate
-        ]
-    )
-
-
-magnitudes : List Vsn.Version -> List Vsn.Magnitude
-magnitudes versions =
-  let
-    versionZero =
-      (0, 0, 0)
-  in
-    List.map2
-      Vsn.magnitude
-      (versionZero :: versions)
-      versions
-
-
-versionSpacing : Vsn.Magnitude -> Float
-versionSpacing magnitude =
-  let
-    gap =
-      8
-
-    versionWidth =
-      Constants.dotSize magnitude Constants.WithBorder
-  in
-    (toFloat (versionWidth + gap)) / Constants.activeWidth
-
-
-timelineSpacingByVersion : List History.Release -> Dict.Dict Vsn.Version Float
-timelineSpacingByVersion history =
-  let
-    versions =
-      List.sortBy .date history
-        |> List.map .version
-
-    widths =
-      List.map versionSpacing (magnitudes versions)
-  in
-    List.map2 (,) versions widths
-      |> Dict.fromList
-
-
-latestInterestingVersions : History.History -> (Vsn.Version, Vsn.Version)
-latestInterestingVersions history =
-  let
-    interestingVersions =
-      Vsn.filterInteresting (List.map .version history)
-  in
-    case List.reverse interestingVersions of
-      latest :: sndLatest :: _ ->
-        ( sndLatest, latest )
-
-      [latest] ->
-        ( Vsn.one, latest )
-
-      [] ->
-        Debug.crash "How can there be a published package with no versions?"
-
+  ( Model
+      history
+      (Dict.fromList (List.map (\h -> (.version h) => Loading) history))
+  , Fx.batch
+      (List.map (loadDocs context << .version) history)
+  )
 
 
 -- UPDATE
 
 
 type Action
-    = UpdateSlider1 Slider.Action
-    | UpdateSlider2 Slider.Action
-    | DocsFailed Vsn.Version
+    = DocsFailed Vsn.Version
     | DocsLoaded Vsn.Version (Docs.Package Type.Type)
 
 
 update : Ctx.OverviewContext -> Action -> Model -> ( Model, Fx.Effects Action )
 update context action model =
   case action of
-    UpdateSlider1 act ->
-      let
-        (newSlider, fx, maybeTarget) =
-          Slider.update model.versions act model.slider1
-
-        (newDocs, maybeRequest) =
-          maybeLoadDocs context model.docs maybeTarget
-      in
-        ( { model | slider1 = newSlider, docs = newDocs }
-        , Fx.batch [ Fx.map UpdateSlider1 fx, maybeRequest ]
-        )
-
-    UpdateSlider2 act ->
-      let
-        (newSlider, fx, maybeTarget) =
-          Slider.update model.versions act model.slider2
-
-        (newDocs, maybeRequest) =
-          maybeLoadDocs context model.docs maybeTarget
-      in
-        ( { model | slider2 = newSlider, docs = newDocs }
-        , Fx.batch [ Fx.map UpdateSlider2 fx, maybeRequest ]
-        )
-
     DocsFailed vsn ->
       ( { model | docs = Dict.insert vsn (Failed 1) model.docs }
       , Fx.none
@@ -183,23 +76,6 @@ update context action model =
       ( { model | docs = Dict.insert vsn (Ready vsnDocs) model.docs }
       , Fx.none
       )
-
-
-
--- SLIDERS
-
-
-sliderInfo : Prox.ProximityTree Vsn.Version -> Slider.Model -> (Float, Vsn.Version)
-sliderInfo versions slider =
-  let
-    fraction =
-      Slider.currentFraction slider
-
-    (_, version) =
-      Prox.nearest fraction versions
-  in
-    (fraction, version)
-
 
 
 -- EFFECTS
@@ -213,29 +89,6 @@ loadDocs context version =
     |> Fx.task
 
 
-maybeLoadDocs
-    : Ctx.OverviewContext
-    -> Dict.Dict Vsn.Version Docs
-    -> Maybe Vsn.Version
-    -> (Dict.Dict Vsn.Version Docs, Fx.Effects Action)
-maybeLoadDocs context docs maybeTarget =
-  case maybeTarget of
-    Nothing ->
-      (docs, Fx.none)
-
-    Just version ->
-      case Dict.get version docs of
-        Nothing ->
-          (Dict.insert version Loading docs, loadDocs context version)
-
-        Just (Failed _) ->
-          (Dict.insert version Loading docs, loadDocs context version)
-
-        Just _ ->
-          (docs, Fx.none)
-
-
-
 -- VIEW
 
 
@@ -243,52 +96,28 @@ maybeLoadDocs context docs maybeTarget =
 
 
 view : Signal.Address Action -> Model -> Html
-view address {history, versions, slider1, slider2, docs} =
+view address {history, docs} =
   let
-    (fraction1, version1) =
-      sliderInfo versions slider1
-
-    (fraction2, version2) =
-      sliderInfo versions slider2
-
-    viewSlider tag frac vsn color =
-      Slider.view
-        (Signal.forwardTo address tag)
-        frac
-        (Vsn.vsnToString vsn)
-        color
-  in
-    div []
-      [ History.view versions
-      , div [ class "slider-container" ]
-          [ viewSlider UpdateSlider1 fraction1 version1 "#7FD13B"
-          , viewSlider UpdateSlider2 fraction2 version2 "#60B5CC"
-          ]
-      , div [ class "diff" ]
-          [ diffHeader fraction1 fraction2 version1 version2
+    diffView version1 version2 =
+      div []
+          [ diffHeader version1 version2
           , lazy3 coarseDiff history version1 version2
           , lazy3 detailedDiff docs version1 version2
           ]
-      ]
+    vs =
+      List.map .version history
 
-
-diffHeader : Float -> Float -> Vsn.Version -> Vsn.Version -> Html
-diffHeader fraction1 fraction2 version1 version2 =
-  let
-    text1 =
-      vsnText "#7FD13B" version1
-
-    text2 =
-      vsnText "#60B5CC" version2
-
-    (leftVsn, rightVsn) =
-      if fraction1 < fraction2 then
-        (text1, text2)
-
-      else
-        (text2, text1)
+    v2s =
+      List.drop 1 vs
   in
-    h1 [] [ text "Changes between ", leftVsn, text " and ", rightVsn ]
+    div []
+      (List.map2 diffView v2s vs)
+
+
+diffHeader : Vsn.Version -> Vsn.Version -> Html
+diffHeader version1 version2 =
+  h1 []
+     [ text (Vsn.vsnToString version1) ]
 
 
 vsnText : String -> Vsn.Version -> Html
@@ -302,7 +131,6 @@ vsnText color vsn =
     ]
 
 
-
 -- VIEW DIFFS
 
 
@@ -312,11 +140,7 @@ coarseDiff history version1 version2 =
       History.diff version1 version2 history
   in
     div
-      [ style
-          [ "text-align" => "center"
-          , "padding" => "16px 0"
-          ]
-      ]
+      []
       [ coarseDiffChunk ("Added " ++ toString added)
       , coarseDiffChunk ("Changed " ++ toString changed)
       , coarseDiffChunk ("Removed " ++ toString removed)
@@ -346,7 +170,7 @@ detailedDiff docs version1 version2 =
         text "Loading.."
 
       _ ->
-        text ""
+        text "UNEXPECTED STATE"
 
 
 detailedDiffHelp docs pkg1 pkg2 =
